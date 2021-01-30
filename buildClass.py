7aa2814f7dc73baf2da79ef655a48393f0d8091a
Ben Carpenter <bacarpenter04@gmail.com>
2021-01-29 22:31:28 -0500

    v1 done. Builds class with constructor.

--- buildClass.py
+++ buildClass.py
@@ -34,8 +34,37 @@ def main():
 
 def buildClass(className, instanceVarList):
     jDocComment = f"/**\n * {className} class built with the build-class python script\n * https://github.com/bacarpenter/build-class\n */"
+    classHeader = f"\npublic class {className} {{\n"
+
     with open(f"{OUTPUT_DIR}/{className}.java", "w+") as classFile:
+
+        # Boiler Plate Code:
         classFile.write(jDocComment)
+        classFile.write(classHeader)
+
+        # Instance Variables:
+        for iv in instanceVarList:
+            classFile.write(f"\tprivate {iv['type']} {iv['name']};\n")
+
+        classFile.write("\n\t// Constructors\n")
+
+        # Constructors:
+        constructorTemplate = f"\t{className}("
+        for iv in instanceVarList[:-1]:
+            constructorTemplate += f"{iv['type']} new{iv['name'].capitalize()},"
+
+        iv2 = instanceVarList[-1]  # Last element
+        constructorTemplate += f"{iv2['type']} new{iv2['name'].capitalize()}"
+        constructorTemplate += ")\n\t{\n"
+
+        for iv in instanceVarList:
+            constructorTemplate += f"\t\t{iv['name']} = new{iv['name'].capitalize()};\n"
+
+        constructorTemplate += "\t}\n"
+        classFile.write(constructorTemplate)
+
+        # End class
+        classFile.write("}")
 
 
 if __name__ == "__main__":
---
+++ testBuildClass.py
@@ -0,0 +1,5 @@
+from buildClass import buildClass
+
+instanceVarList = [{'name': "name", 'type': "String"},
+                   {'name': "age", 'type': "int"}]
+buildClass("TestClass", instanceVarList)
